a1174d29137baeb231b89e8e1e5ed54758667703
gitthatuhlean <75182679+gitthatuhlean@users.noreply.github.com>
2021-01-05 12:41:55 -0600

    updated code 2/5

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,46 @@
 #Open the pybank file
 import os
 import csv
-# import pandas as pd
 
 #empty dictionaries to hold values we will need
-total_months = 0
-# Total = dict()
-# Average_Change = dict()
-# Greatest Increase in Profits = dict()
-# Greatest Decrease in Profits = dict()
+num_of_rows = 0
+total = 0
 
 #path to file from where code is in my folders
 pybank_csv = os.path.join("Resources","budget_data.csv")
-
+# df=pybank_csv
 #open file for read
 with open(pybank_csv,"r") as budget:
-    csvreader = csv.reader(budget,delimiter=',')
+    csv_reader = csv.reader(budget,delimiter=',')
 
-#total months included in the dataset
+#get rid of header and prints it 
+    csv_header = next(budget)
 
-#get rid of header and prints it
-    csv_header = next(csvreader)
-    #print(f"Header:{csv_header}")
+#find total number of months last row index for total # of months of data question-HELP HERE won't add
+# def convert_tolist(csv_reader):
+#     csv_reader_delim = csv_reader.split("n") 
+#     csv_reader=list(csv_reader(csv_reader_delim))
+
+    for row in csv_reader:
+#converting values to int 
+        date_num= row[1]
+        date_num=int(date_num)
+        row[1]=date_num
+#add up total values, total value of the profits and losses column
+        date_num= row[1]
+        total += date_num
+#add up number of rows
+       num_of_rows+= 1   
+# calculate average
+        if 
+print(f"Total Months: {num_of_rows}") 
+print(f"Total: {total}")
+ 
     
-    total_months = len(list(csvreader))
-    print(total_months)
 
-#get rid of header and prints it
-    # csv_header = next(csvreader)
-    # print(f"Header:{csv_header}")
 
-# find total number of last row index for total # of months of data question
 
+
+# total # of months
+
+       
